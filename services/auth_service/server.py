f5a4e04e5ef246fd6fd298afd890e719611fe2d2
Stanislav Bobokalo <stas.gurland@gmail.com>
2024-04-27 17:59:59 +0300

    fix: use configuration variables from env

--- services/auth_service/server.py
+++ services/auth_service/server.py
@@ -3,11 +3,11 @@ import asyncio
 from grpclib.utils import graceful_exit
 from grpclib.server import Server
 from auth_service.service import AuthService
+from auth_service.cfg import SERVICE_PORT, SERVICE_HOST
 
 
-async def main(*, host: str = '127.0.0.1', port: int = 50051) -> None:
+async def main(host: str = '127.0.0.1', port: int = 50051) -> None:
     server = Server([AuthService()])
-    # Note: graceful_exit isn't supported in Windows
     with graceful_exit([server]):
         await server.start(host, port)
         print(f'Serving on {host}:{port}')
@@ -15,4 +15,7 @@ async def main(*, host: str = '127.0.0.1', port: int = 50051) -> None:
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(
+        host=SERVICE_HOST,
+        port=SERVICE_PORT
+    ))
