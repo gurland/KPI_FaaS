7a472b4d7368e77fea61f81c27d5d1e9fcbb5ed7
Stanislav Bobokalo <stas.gurland@gmail.com>
2024-04-28 15:48:42 +0300

    feat: add ChangeUserPassword rpc

--- services/auth_service/debug_client.py
+++ services/auth_service/debug_client.py
@@ -1,5 +1,5 @@
 import asyncio
-from auth_service.contracts.faas import AuthServiceStub, UserCredentialsRequest
+from auth_service.contracts.faas import AuthServiceStub, UserCredentialsRequest, ChangeUserPasswordRequest
 
 from grpclib.client import Channel
 
@@ -8,19 +8,30 @@ async def main():
     channel = Channel(host="127.0.0.1", port=50051)
     auth_service = AuthServiceStub(channel)
 
-    response = await auth_service.create_user(
-        UserCredentialsRequest(
-            username="user",
-            password="pass"
+    try:
+        response = await auth_service.create_user(
+            UserCredentialsRequest(
+                username="test",
+                password="test"
+            )
         )
-    )
 
-    print(response.role)
+        print(response)
+    except:
+        pass
 
     response = await auth_service.get_user(
         UserCredentialsRequest(
-            username="user",
-            password="pass"
+            username="test",
+            password="test"
+        )
+    )
+
+    response = await auth_service.change_user_password(
+        ChangeUserPasswordRequest(
+            user_id=response.user_id,
+            old_password="test",
+            new_password="test"
         )
     )
 
