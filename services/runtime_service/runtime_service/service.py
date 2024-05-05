5eeb726d4945ad3627661dde9c012f6f7db4ef0a
Stanislav Bobokalo <stanislav.b@embedd.it>
2024-05-05 10:59:36 +0300

    fix: add support for server stream RPC

--- services/runtime_service/runtime_service/service.py
+++ services/runtime_service/runtime_service/service.py
@@ -1,4 +1,4 @@
-from typing import AsyncIterator, Dict, Callable
+from typing import AsyncIterator, Dict, Callable, AsyncIterable
 import tarfile
 import io
 
@@ -27,6 +27,23 @@ class RuntimeService(RuntimeServiceBase):
 
         return f
 
+    def rpc_call_with_metadata_server_stream(self, handler: Callable):
+        async def f(stream: grpclib.server.Stream):
+            request = await stream.recv_message()
+
+            response_iter = handler(request, metadata=stream.metadata)
+            # check if response is actually an AsyncIterator
+            # this might be false if the method just returns without
+            # yielding at least once
+            # in that case, we just interpret it as an empty iterator
+            if isinstance(response_iter, AsyncIterable):
+                async for response_message in response_iter:
+                    await stream.send_message(response_message)
+            else:
+                response_iter.close()
+
+        return f
+
     def __mapping__(self) -> Dict[str, grpclib.const.Handler]:
         existing_mapping = super().__mapping__()
         new_mapping = {}
@@ -37,9 +54,14 @@ class RuntimeService(RuntimeServiceBase):
             handler_method_name = __rpc_func.__name__.removeprefix("__rpc_")
             handler_method = getattr(self, handler_method_name)
 
-            new_mapping[route] = grpclib.const.Handler(
-                self.rpc_call_with_metadata(handler_method), cardinality, request_type, reply_type
-            )
+            if cardinality == grpclib.const.Cardinality.UNARY_STREAM:
+                new_mapping[route] = grpclib.const.Handler(
+                    self.rpc_call_with_metadata_server_stream(handler_method), cardinality, request_type, reply_type
+                )
+            else:
+                new_mapping[route] = grpclib.const.Handler(
+                    self.rpc_call_with_metadata(handler_method), cardinality, request_type, reply_type
+                )
 
         return new_mapping
 
