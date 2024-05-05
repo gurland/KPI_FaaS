88ae403048384e5d3e25188dab435038661b6b45
Stanislav Bobokalo <stanislav.b@embedd.it>
2024-05-05 10:58:05 +0300

    feat: add logs collection into DockerImage

--- services/runtime_service/runtime_service/docker_image.py
+++ services/runtime_service/runtime_service/docker_image.py
@@ -1,3 +1,4 @@
+import json
 import tarfile
 import io
 
@@ -14,20 +15,20 @@ class DockerImage:
     docker_client: DockerClient
 
     image: Image | None = None
-    registry_url: str | None = None
+    base_registry_url: str | None = None
+    logs: list
 
-    def __init__(self, tag: str, dockerfile: str, registry_url: str) -> None:
+    def __init__(self, tag: str, dockerfile: str, base_registry_url: str) -> None:
         self.tag = tag
         self.dockerfile = dockerfile
         self.docker_client = docker.from_env()
-        self.registry_url = registry_url
+        self.base_registry_url = base_registry_url
+        self.logs = []
 
         self.docker_client.login(
             username=None, password=None, email=None,
-            registry=registry_url
+            registry=base_registry_url
         )
-
-        pass
 
     def build(self):
         build_context = self.create_docker_build_context_tarball(self.dockerfile)
@@ -39,15 +40,23 @@ class DockerImage:
         )
         self.image = image
 
-        logger.info(f"Built docker image with tag: '{self.tag}'. Build log: {list(logs_generator)}")
+        build_logs = list(json.dumps(log_line) for log_line in logs_generator)
+        self.logs.extend(build_logs)
 
-    def push(self) -> None:
-        self.image.tag(f"{self.registry_url}/{self.tag}")
+        logger.info(f"Built docker image with tag: '{self.tag}'. Build logs: {build_logs}")
+
+    def push(self) -> str:
+        self.image.tag(f"{self.base_registry_url}/{self.tag}")
         response = self.docker_client.images.push(
-            repository=f"{self.registry_url}/{self.tag}"
+            repository=f"{self.base_registry_url}/{self.tag}"
         )
 
-        logger.info(f"Response for push: {response}")
+        push_logs = response.splitlines()
+        self.logs.extend(push_logs)
+        logger.info(f"Pushed image {self.image.tags}. Response: {push_logs}")
+
+        return f"{self.base_registry_url}/{self.tag}"
+
 
     @staticmethod
     def create_docker_build_context_tarball(dockerfile_content: str) -> io.BytesIO:
