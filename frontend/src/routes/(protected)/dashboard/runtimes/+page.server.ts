ea503a9e6eac668cf326662012e4d43b4a23f132
yellott <16868660+yellott@users.noreply.github.com>
2024-05-06 09:00:20 +0300

    Remove redundant code

--- frontend/src/routes/(protected)/dashboard/runtimes/+page.server.ts
+++ frontend/src/routes/(protected)/dashboard/runtimes/+page.server.ts
@@ -1,5 +1,4 @@
-import { fail, type ActionFailure, type Redirect } from '@sveltejs/kit';
-import type { Actions, PageServerLoadEvent, RequestEvent } from './$types';
+import type { PageServerLoadEvent } from './$types';
 import { getRpcMetaData, runtimeService } from '@/server';
 
 export async function load(event: PageServerLoadEvent) {
@@ -12,42 +11,3 @@ export async function load(event: PageServerLoadEvent) {
 		runtimes
 	};
 }
-
-type CreateRuntimeFormData = {
-	tag: FormDataEntryValue;
-	dockerfile: FormDataEntryValue;
-	errorMessage?: string;
-};
-
-export const actions: Actions = {
-	createRuntime: async (
-		event: RequestEvent
-	): Promise<CreateRuntimeFormData | ActionFailure<CreateRuntimeFormData> | Redirect> => {
-		const { request } = event;
-		const signupFormData = await request.formData();
-		const tag = signupFormData.get('tag') ?? '';
-		const dockerfile = signupFormData.get('dockerfile') ?? '';
-
-		const createRuntimeResponse: CreateRuntimeFormData = {
-			tag,
-			dockerfile
-		};
-
-		try {
-			await runtimeService.createRuntime(
-				{
-					tag: tag.toString(),
-					dockerfile: dockerfile.toString()
-				},
-				{ metadata: getRpcMetaData(event) }
-			);
-		} catch (e) {
-			if (e instanceof Error) {
-				console.error(e);
-				return fail(400, { ...createRuntimeResponse, errorMessage: e.message });
-			}
-		}
-
-		return createRuntimeResponse;
-	}
-};
