e445a789ed1a33f16aa7d8b799551cee8f32fe4d
yellott <16868660+yellott@users.noreply.github.com>
2024-05-10 11:16:59 +0300

    Fix rpc authMiddleware

--- frontend/src/lib/server/services/middlewares/authMiddleware.ts
+++ frontend/src/lib/server/services/middlewares/authMiddleware.ts
@@ -2,8 +2,8 @@ import type { User } from 'lucide-svelte';
 import { Metadata, type ClientMiddleware } from 'nice-grpc';
 
 export const authMiddleware: ClientMiddleware = async function* (call, options) {
-	const userMetaData = options.metadata?.get('user') ?? '{"user-id": null}';
-	const user = JSON.parse(userMetaData) as User;
+	const userMetaData = options.metadata?.get('user') ?? '{"userId": null}';
+	const user = (JSON.parse(userMetaData) as User) ?? { userId: '' };
 
 	return yield* call.next(call.request, {
 		metadata: new Metadata({ ['user-id']: user.userId.toString() })
