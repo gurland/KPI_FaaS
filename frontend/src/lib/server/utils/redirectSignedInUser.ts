b743b9d12301c235d639f42a8046453ac5cfd0be
yellott <16868660+yellott@users.noreply.github.com>
2024-05-10 10:04:23 +0300

    Fix signed user redirect url

--- frontend/src/lib/server/utils/redirectSignedInUser.ts
+++ frontend/src/lib/server/utils/redirectSignedInUser.ts
@@ -1,6 +1,6 @@
 import { redirect, type RequestEvent } from '@sveltejs/kit';
 
 export const redirectSignedInUser = (event: RequestEvent) => {
-	const redirectTo = event.url.searchParams.get('redirectTo') || 'dashboard';
+	const redirectTo = event.url.searchParams.get('redirectTo') || '';
 	return redirect(302, `/${redirectTo}`);
 };
