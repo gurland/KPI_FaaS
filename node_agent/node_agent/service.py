93642fbdf3a9d122ebe6574c4fef80df2909cd82
Stanislav Bobokalo <stanislav.b@embedd.it>
2024-06-11 00:32:58 +0300

    feat: add initial agent service

--- node_agent/node_agent/service.py
+++ node_agent/node_agent/service.py
@@ -1,14 +1,63 @@
-import datetime
-
-from grpclib import GRPCError, Status
-from sqlalchemy.orm import Session
-
-from .contracts.faas import AgentServiceBase, InvokeFunctionRequest, InvocationResult
+import docker
 
 
-class AuthService(AgentServiceBase):
+from .contracts.faas import AgentServiceBase, InvokeFunctionRequest, InvocationResult, Logs
+
+import io
+import json
+import sys
+
+
+def call_function_from_string(code: str, context: dict) -> dict:
+    old_stdout, sys.stdout = sys.stdout, io.StringIO()
+
+    function_globals, function_locals = {}, {}
+    exec(code, function_globals, function_locals)
+
+    function_result = function_locals["lambda_function"](context)
+
+    stdout_content = sys.stdout.getvalue()
+    sys.stdout = old_stdout
+
+    return {
+        "result": function_result,
+        "logs": stdout_content.splitlines()
+    }
+
+
+class AgentService(AgentServiceBase):
     async def invoke_function(
             self,
             request: InvokeFunctionRequest
     ) -> InvocationResult:
-        request
+        client = docker.from_env()
+        client.login(
+            username=None, password=None, email=None,
+            registry="localhost:5000"
+        )
+        container = client.containers.run(request.runtime.tag, detach=True, ports={'9999/tcp': 9999})
+        # try:
+        #     client = Client(("localhost", 9999))
+        #     context = json.loads(request.json_trigger_context)
+        #     client.send(
+        #         {
+        #             "code": request.function.code,
+        #             "context": context
+        #         }
+        #     )
+        #
+        #     result = client.recv()
+        #
+        # finally:
+        #     container.stop()
+        #     container.remove()
+        #     client.close()
+
+        result = call_function_from_string(request.function.code, json.loads(request.json_trigger_context))
+
+        return InvocationResult(
+            json=json.dumps(result.get("result", {})),
+            log_lines=Logs(
+                log_lines=result.get("logs", ["1", "2"])
+            )
+        )
--- node_agent/server.py
+++ node_agent/server.py
@@ -2,12 +2,12 @@ import asyncio
 
 from grpclib.utils import graceful_exit
 from grpclib.server import Server
-from auth_service.service import AuthService
-from auth_service.cfg import SERVICE_PORT, SERVICE_HOST
+from node_agent.service import AgentService
+from node_agent.cfg import SERVICE_PORT, SERVICE_HOST
 
 
 async def main(host: str = '127.0.0.1', port: int = 50051) -> None:
-    server = Server([AuthService()])
+    server = Server([AgentService()])
     with graceful_exit([server]):
         await server.start(host, port)
         print(f'Serving on {host}:{port}')
