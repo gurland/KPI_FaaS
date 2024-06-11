4fc49fc180fa5c22d3a7b64c2d5480d388b481db
Stanislav Bobokalo <stanislav.b@embedd.it>
2024-06-11 22:36:08 +0300

    fix: add better example to test service function invocation

--- services/load_balancer_service/debug_client.py
+++ services/load_balancer_service/debug_client.py
@@ -12,19 +12,26 @@ channel = Channel(host=SERVICE_HOST, port=SERVICE_PORT)
 service = LoadBalancerServiceStub(channel)
 
 
+demo_f = """def lambda_function(context: dict) -> dict:
+    message = "Hello, cruel, serverless world :("
+    print(message)
+
+    return {"message": message}
+"""
+
 async def main():
     try:
         response = await service.invoke_function(
             InvokeFunctionRequest(
                 function=DetailedFunction(
                     function_id=1,
-                    runtime_tag="tag",
+                    runtime_tag="runtimes/python:3.11",
                     function_name="Test function",
-                    code="empty",
+                    code=demo_f,
                 ),
                 runtime=BriefRuntime(
-                    tag="tag",
-                    registry_url="localhost"
+                    tag="runtimes/python:3.11",
+                    registry_url="localhost:5555/runtimes/python:3.11"
                 ),
                 json_trigger_context=json.dumps({"Test": "Hello"})
             ),
