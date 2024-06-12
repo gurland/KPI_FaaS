d65451815464f32ea65192cf3244d24a37b05803
Stanislav Bobokalo <stanislav.b@embedd.it>
2024-06-12 03:46:27 +0300

    feat: add setupable docker registry url

--- node_agent/node_agent/cfg.py
+++ node_agent/node_agent/cfg.py
@@ -1,4 +1,9 @@
 import os
+from logging import Logger
+
+logger = Logger("node_agent")
 
 SERVICE_HOST = os.getenv("SERVICE_HOST", "0.0.0.0")
 SERVICE_PORT = os.getenv("SERVICE_PORT", "50000")
+
+DOCKER_REGISTRY_URL = os.getenv("DOCKER_REGISTRY_URL", "localhost:5555")
