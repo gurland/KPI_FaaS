eb08bbdeb60c30a38d14334787caa70032a92e76
Stanislav Bobokalo <stanislav.b@embedd.it>
2024-06-12 09:42:34 +0300

    fix: update demo

--- demo/ASAMPL/demo_invoker.py
+++ demo/ASAMPL/demo_invoker.py
@@ -11,28 +11,29 @@ if __name__ == '__main__':
         ("0.0.0.0", 9999)
     )
 
-    while conn := listener.accept():
-        try:
-            data = json.loads(conn.recv())
-            code, context = data.get("code"), data.get("context")
+    while True:
+        while conn := listener.accept():
+            try:
+                data = json.loads(conn.recv())
+                code, context = data.get("code"), data.get("context")
 
-            with open("function.asa", "w") as f:
-                f.write(code)
+                with open("function.asa", "w") as f:
+                    f.write(code)
 
-            source = os.path.dirname(__file__)
-            asampl_path = os.path.join(source, 'asampl')
+                source = os.path.dirname(__file__)
+                asampl_path = os.path.join(source, 'asampl')
 
-            logs = subprocess.check_output(f'{asampl_path} function.asa', shell=True).decode()
+                logs = subprocess.check_output(f'{asampl_path} function.asa', shell=True).decode()
 
-            conn.send(
-                json.dumps({
-                    "result": {},
-                    "logs": logs.splitlines()
-                })
-            )
-            conn.close()
+                conn.send(
+                    json.dumps({
+                        "result": {},
+                        "logs": logs.splitlines()
+                    })
+                )
+                conn.close()
 
-        finally:
-            conn.close()
+            finally:
+                conn.close()
 
     listener.close()
