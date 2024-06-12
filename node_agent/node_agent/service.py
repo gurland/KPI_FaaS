a11093da18681af9ccbe354be087a1018c9a3ec4
Stanislav Bobokalo <stanislav.b@embedd.it>
2024-06-12 03:47:15 +0300

    feat: add proper function invocation and runtime container launch and network management

--- node_agent/node_agent/service.py
+++ node_agent/node_agent/service.py
@@ -1,28 +1,38 @@
+from asyncio import sleep
+from multiprocessing.connection import Client
+
 import docker
+from docker import DockerClient
+from docker.errors import APIError
+from docker.models.containers import Container
+from docker.models.networks import Network
 
-
+from .cfg import DOCKER_REGISTRY_URL, logger
 from .contracts.faas import AgentServiceBase, InvokeFunctionRequest, InvocationResult, Logs
 
-import io
 import json
-import sys
+import hashlib
 
 
-def call_function_from_string(code: str, context: dict) -> dict:
-    old_stdout, sys.stdout = sys.stdout, io.StringIO()
+def get_launched_runtimes(client: DockerClient) -> dict[str, Container]:
+    result = {}
+    for container in client.containers.list():
+        if not container.name.startswith("runtime-container"):
+            continue
 
-    function_globals, function_locals = {}, {}
-    exec(code, function_globals, function_locals)
+        for tag in container.image.tags:
+            result[tag] = container
 
-    function_result = function_locals["lambda_function"](context)
+    return result
 
-    stdout_content = sys.stdout.getvalue()
-    sys.stdout = old_stdout
 
-    return {
-        "result": function_result,
-        "logs": stdout_content.splitlines()
-    }
+def get_container_network(client: DockerClient, container: Container) -> Network:
+    network_ids = list(container.attrs.get('NetworkSettings', {}).get("Networks", {}).keys())
+    return client.networks.get(network_ids[0])
+
+
+def get_tag_md5(runtime_tag: str) -> str:
+    return hashlib.md5(runtime_tag.encode()).hexdigest()
 
 
 class AgentService(AgentServiceBase):
@@ -33,31 +43,78 @@ class AgentService(AgentServiceBase):
         client = docker.from_env()
         client.login(
             username=None, password=None, email=None,
-            registry="localhost:5555"
+            registry=DOCKER_REGISTRY_URL
         )
-        container = client.containers.run(request.runtime.tag, detach=True, ports={'9999/tcp': 9999})
-        # try:
-        #     client = Client(("localhost", 9999))
-        #     context = json.loads(request.json_trigger_context)
-        #     client.send(
-        #         {
-        #             "code": request.function.code,
-        #             "context": context
-        #         }
-        #     )
-        #
-        #     result = client.recv()
-        #
-        # finally:
-        #     container.stop()
-        #     container.remove()
-        #     client.close()
+        launched_runtimes = get_launched_runtimes(client)
+        safe_tag = get_tag_md5(request.runtime.tag)
 
-        result = call_function_from_string(request.function.code, json.loads(request.json_trigger_context))
+        if request.runtime.tag not in launched_runtimes:
+            try:
+                network = client.networks.create(
+                    f"runtime-network-{safe_tag}",
+                    driver="bridge",
+                    attachable=True
+                )
+                logger.info(f"Created network {network.name}")
+            except APIError:
+                network = client.networks.get(f"runtime-network-{safe_tag}")
+                logger.info(f"Got already existing network {network.name}")
+
+            try:
+                runtime_container = client.containers.run(
+                    request.runtime.tag,
+                    detach=True,
+                    name=f"runtime-container-{safe_tag}",
+                    network=network.name
+                )
+                logger.info(f"Started runtime container. {runtime_container.name} | Tag: {request.runtime.tag}")
+            except APIError:
+                runtime_container = client.containers.get(
+                    f"runtime-container-{safe_tag}"
+                )
+                runtime_container.remove()
+                runtime_container = client.containers.run(
+                    request.runtime.tag,
+                    detach=True,
+                    name=f"runtime-container-{safe_tag}",
+                    network=network.name
+                )
+                logger.info(f"Re-Started runtime container. {runtime_container.name} | Tag: {request.runtime.tag}")
+        else:
+            runtime_container = launched_runtimes[request.runtime.tag]
+
+        runtime_network = get_container_network(client, runtime_container)
+        agent_container = client.containers.get("kpi_faas-node-agent")
+
+        try:
+            runtime_network.connect(agent_container)
+        except APIError:
+            logger.info(f"Agent is already connected to network {agent_container}")
+
+        result = {}
+        for _ in range(10):
+            try:
+                client = Client((runtime_container.name, 9999))
+                client.send(
+                    json.dumps({
+                        "code": request.function.code,
+                        "context": request.json_trigger_context
+                    })
+                )
+
+                result = client.recv()
+                break
+            except:
+                await sleep(1)
+
+        client.close()
+        logger.info(runtime_container.logs())
+
+        result = json.loads(result)
 
         return InvocationResult(
             json=json.dumps(result.get("result", {})),
             log_lines=Logs(
-                log_lines=result.get("logs", ["1", "2"])
+                log_lines=result.get("logs", ["Some error happened"])
             )
         )
